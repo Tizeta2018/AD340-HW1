fd7b7d0908db255d0a58fac264f5d325775426b2
Tizeta2018 <tesfutizeta@yahoo.com>
2021-06-16 22:38:32 -0700

    HW6

---
+++ app/src/main/java/com/example/myapplicationhw3/Cam.kt
@@ -0,0 +1,20 @@
+package com.example.myapplicationhw3
+
+data class Cam(val description: String, private val image: String, var type: String, var coords: DoubleArray ) {
+
+    private val baseUrl: Map<String, String> = mapOf(
+        "sdot" to "https://www.seattle.gov/trafficcams/images/",
+        "wsdot" to "https://images.wsdot.wa.gov/nw/"
+    )
+
+    fun imageUrl() : String {
+        return baseUrl[this.type] + this.image
+    }
+
+    companion object {
+        var camUrl = " https://web6.seattle.gov/Travelers/api/Map/Data?zoomId=13&type=2"
+
+
+
+    }
+}
